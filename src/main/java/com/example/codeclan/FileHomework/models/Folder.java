e8c805033450fbd0ce4a4f17d87761b6898858a6
fddata <fraserjdouglas@gmail.com>
2018-10-09 16:30:06 +0100

    adds Folder class

---
+++ src/main/java/com/example/codeclan/FileHomework/models/Folder.java
@@ -0,0 +1,52 @@
+package com.example.codeclan.FileHomework.models;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public void affFile(File file){
+        this.files.add(file);
+    }
+}
